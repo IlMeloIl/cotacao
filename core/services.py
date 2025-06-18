2d54d7ebed2687007dc91c25c2f9f938c42bffb1
IlMeloIl <jgvmelo123@gmail.com>
2025-06-18 10:45:10 -0300

    service para consumir API Vatcomply

---
+++ core/services.py
@@ -0,0 +1,23 @@
+import requests
+from datetime import date
+
+class VatcomplyService:
+    BASE_URL = "https://api.vatcomply.com/rates"
+
+    def get_rates(self, target_date, symbols=None):
+        date_str = target_date.strftime('%Y-%m-%d')
+        url = f"{self.BASE_URL}?date={date_str}&base=USD"
+
+        if symbols:
+            symbols_str = ','.join(symbols)
+            url += f'&symbols={symbols_str}'
+
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+
+            data = response.json()
+            return data.get('rates', {})
+        except requests.exceptions.RequestException as e :
+            print(f"Erro ao acesar API da Vatcomply: {e}")
+            return None
